896debb097a9bdc9ea1c908ba2383ad9274e0477
Pranchal15 <pranchal.agarwal@gmail.com>
2022-06-18 12:54:05 +0530

    tried scss

---
+++ global_UI/button.js
@@ -0,0 +1,10 @@
+
+import * as styles from './Button.module.scss'
+
+export default function Button() {
+  return (
+    <button className={styles.button}>
+        HI
+    </button>
+  )
+}
--- pages/homePage.js
+++ pages/homePage.js
@@ -1,9 +1,14 @@
 import { getUsers,fun } from "../services/homePageServices"
 import { useEffect } from "react";
+import Button from "../global_UI/button";
 
 export default function homePage() {
 //homePage with side nav
   return (
-    <div>homePage</div>
+    
+    <div><p>Home Page</p>
+    <Button/>
+    
+    </div>
   )
 }
